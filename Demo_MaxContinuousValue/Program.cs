d6f1c938eb3e760fa4c8a6613f5c7f47838bb67a
伯翰 侯 <john860703@gmail.com>
2022-10-19 16:10:01 +0800

    加入專案檔案。

---
+++ Demo_MaxContinuousValue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_MaxContinuousValue
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//陣列裡只會有0、3，找出連續3數的總和
+
+
+		}
+	}
+}
